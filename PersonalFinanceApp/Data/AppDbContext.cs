441c69010259a46a8b44892530298affdf720a78
Tomislav Prstec <toprstec@vsite.hr>
2025-01-02 21:32:12 +0100

    Added register and login logic to application.

---
+++ PersonalFinanceApp/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PersonalFinanceApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    }
+}
