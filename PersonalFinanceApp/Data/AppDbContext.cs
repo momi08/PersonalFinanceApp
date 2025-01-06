5f53c3cd6ebe2961c54f476b10348c7ad7d58c0d
Tomislav Prstec <toprstec@vsite.hr>
2025-01-06 12:14:09 +0100

    Added dashboard page and changed name of DB to PersonalFinanceDB

--- PersonalFinanceApp/Data/AppDbContext.cs
+++ PersonalFinanceApp/Data/AppDbContext.cs
@@ -5,6 +5,7 @@ namespace PersonalFinanceApp.Data
     public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
