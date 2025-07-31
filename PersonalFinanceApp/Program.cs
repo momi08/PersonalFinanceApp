124a90f7ecc4ca9555786324c40125d760b52f7c
Tomislav Prstec <toprstec@vsite.hr>
2025-07-31 10:35:39 +0200

    Changed MainLayout.razor and colors of it.

--- PersonalFinanceApp/Program.cs
+++ PersonalFinanceApp/Program.cs
@@ -21,17 +21,13 @@ namespace PersonalFinanceApp
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
             ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
 
-            // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
